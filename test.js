d30a4d11198cad4bd2a2bf397a0de1ff4ea982d2
julien mounier <julien.mounier@tanker.io>
2021-06-10 15:19:31 +0200

    Bump @tanker/client-node to 2.15-alpha1 and start Tanker

--- test.js
+++ test.js
@@ -2,8 +2,11 @@ const { Tanker } = require('@tanker/client-node')
 
 describe('Tanker', () => {
   it('should not display a warning', async () => {
-    new Tanker({
-      appId: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopq'
+    const t = new Tanker({
+      appId: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
     })
+
+    const identity = "a wrong identity";
+    await expect(t.start(identity)).rejects.toThrow(`Invalid secret permanent identity provided: ${identity}`);
   });
 });
